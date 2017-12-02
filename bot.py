d93d0ade36de9f4a2e1d20ba4894a9b5a7d00d1f
Miguel Ángel Gámez Hidalgo <miguelangelgh9@hotmail.com>
2017-12-02 12:23:33 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ import telebot
 # Example of your code beginning
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-issue=188
+issue=887
+chats=[]
 #some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
@@ -46,48 +47,98 @@ logger = logging.getLogger(__name__)
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi! I can show you the latest One Piece issue on the MangaStream RSS feed, just use the /feed command.')
-    
+    update.message.reply_text("Hi!\nI was created to send messages to chats whenever a new One Piece issue is released, if you would like to recieve them as well just use /addchat but if you don't want the updates use /rmchat \nI can also look for a specific manga entry in the MangaStream by using /feed <the name of the manga>, just know that you have to write it as it appears on MangaStream and with spaces.\nBy the way, if you stop recieving updates but didnt /rmchat, it might be becase I was killed. Call the cops.")
+
+def addchat(bot, update):
+    """Add the current chat to the list of chats to update"""
+    chatid=update.message.chat_id
+    global chats
+    if chatid in chats:
+        update.message.reply_text("It seems like you are already on the list!")
+    else:
+        chats.append(chatid)
+        update.message.reply_text("Your chat has been added and will be notified once a new update comes!")
+
+def rmchat(bot, update):
+    """Remove the current chat from the list of chats to update"""
+    chatid=update.message.chat_id
+    global chats
+    if chatid in chats:
+        chats.remove(chatid)
+        update.message.reply_text("You have been removed succesfully and won't be notified in the future!")
+    else:
+        update.message.reply_text("Your chat is not in my list... Would you like to /addchat ?")
+
 def currentIssue(bot, update):
     """Send a message when the command /issue is issued."""
-    update.message.reply_text(str(issue))
-    
-"""def caps(bot, update, args):
-    name = ' '.join(args).title()
-    bot.send_message(chat_id=update.message.chat_id, text=text_caps)"""
-    
-def feed(bot, update):
+    update.message.reply_text("The current One Piece issue is " + str(issue))
+
+def feed(bot, update, args):
+    url='https://www.mangastream.com/rss'
+    feed = feedparser.parse(url)
+    name = ' '.join(args).lower()
+    piece=''
+    for i in feed['items']:
+        if name in i['title'].lower():
+            piece=i
+            break
+    if piece:
+        link=piece['links'][0]
+        update.message.reply_text('The latest issue is '+ piece['title']+ '. It was released on ' +piece['published']+'\nYou can read it on MangaStream: '+ link['href'])
+
+    else:
+        update.message.reply_text("I couldn't find anything on the MangaStream RSS feed with that name, maybe you misspelled it?")
+
+def onepiece(bot, update):
     url='https://www.mangastream.com/rss'
     feed = feedparser.parse(url)
     for i in feed['items']:
         if 'One Piece' in i['title']:
             piece=i
-            global issue
-            issue += 1
             break
     link=piece['links'][0]
     update.message.reply_text('The latest One Piece issue is '+ piece['title']+ '. It was released on ' +piece['published']+'\nYou can read it on MangaStream: '+ link['href'])
 
+def alarm(bot, job):
+    url='https://www.mangastream.com/rss'
+    feed = feedparser.parse(url)
+    global issue
+    for piece in feed['items']:
+        if 'One Piece' in piece['title'] and str(issue + 1) in piece['title']:
+            issue += 1
+            link=piece['links'][0]
+            global chats
+            for c in chats:
+                bot.send_message(chat_id=c, text='The latest One Piece issue is '+ piece['title']+ '. It was released on ' +piece['published']+'\nYou can read it on MangaStream: '+ link['href'])
+            break
+
+
 def help(bot, update):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Hey There! For the moment I can only show you the latest One Piece issue on Manga stream, just use /feed to see it.')
+    update.message.reply_text('Hey There!\n These are all my commands:\n/start\n/addchat\n/rmchat\n/onepiece\n/issue\n/feed')
 
 
 def echo(bot, update):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text("Sorry, I don't fully speak human just yet! Could you please use something I can understand? If you need help you can always use /help")
 
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+def unknown(bot, update):
+    update.message.reply_text("I don't know what you are trying to say, do you need /help ?")
+
 
 def main():
     """Start the bot."""
-    
+
     # Create the EventHandler and pass it your bot's token.
     updater = Updater(token)
+
+    #Add the reapeating job to the job queue
+    updater.job_queue.run_repeating(alarm,1800)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -95,9 +146,12 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("feed", feed))
+    dp.add_handler(CommandHandler("onepiece", onepiece))
     dp.add_handler(CommandHandler("issue", currentIssue))
-    #dp.add_handler(CommandHandler('mangaStream', mangaStream, pass_args=True))
+    dp.add_handler(CommandHandler("addchat", addchat))
+    dp.add_handler(CommandHandler("rmchat", rmchat))
+    dp.add_handler(CommandHandler('feed', feed, pass_args=True))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
